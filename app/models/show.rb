563e67b0b91a6d0867ba95bc65927bfaf241e667
Cody Rickman <cody@creativemindstudios.com>
2019-11-12 02:16:23 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -16,14 +16,14 @@ class Show < ActiveRecord::Base
   end
   def self.popular_shows
     shows = []
-    Show.where(rating: 5..10) do |show|
+    Show.where(rating: 5..10).each do |show|
       shows << show
     end
     return shows
   end
   def self.shows_by_alphabetical_order
     shows = []
-    Show.all.order(name: :desc).each do |show|
+    Show.all.order(name: :asc).each do |show|
       shows << show
     end
     return shows
